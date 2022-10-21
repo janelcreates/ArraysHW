aeff06dd8c70293588ff1655d3d351fe75edda44
janelcreates <111800359+janelcreates@users.noreply.github.com>
2022-10-20 18:41:18 -0700

    Create SumofArray.java

---
+++ SumofArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class SumofArray {
+
+	public static void main(String[] args) {
+		int myArray[] = {2, 3, 4, 2};
+		int sum = 0;
+		for (int i = 0; i < myArray.length; i++) {
+			sum += myArray[i];
+			
+			
+		}
+		System.out.println("The sum is: " + sum);
+	}
+
+}
